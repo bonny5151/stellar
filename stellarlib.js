24bc24102012023d491444a9333e252d8888a4b4
bonny5151 <lkjelkjl@alkj.com>
2023-11-21 14:24:01 -0500

    a

--- stellarlib.js
+++ stellarlib.js
@@ -30,7 +30,7 @@ getfee = async function getfee(multiply=1) {
 startnewtransaction = async function startnewtransaction(key, ac)
 {
    if(!ac) { ac = await getaccount(key)}
- var fee = await getfee(2)
+ var fee = await getfee(3)
  var transaction = new StellarSdk.TransactionBuilder(ac, { fee, networkPassphrase: StellarSdk.Networks.PUBLIC })
   return transaction
 }
@@ -46,11 +46,15 @@ fundnewaccount = async function fundnewaccount({key, ac, to, amount}) {
 }
 
 //{transaction , key , ac , to, amount} = 
-send = async function send({key, ac, to, amount})
+send = async function send({key, ac, to, amount, memo})
 {
    var transaction = await startnewtransaction(key, ac)
    var t1=transaction.addOperation(StellarSdk.Operation.payment({
-               destination: to, asset: sdk.Asset.native(), amount: amount + ""})).setTimeout(3000).build()
+               destination: to, asset: StellarSdk.Asset.native(), amount: amount + ""}))
+   if(memo) { 
+     t1 = t1.addMemo(StellarSdk.Memo.text(memo))
+    }
+   t1 = t1.setTimeout(3000).build()
    t1.sign(key)
    var r=0; try{ r = await server.submitTransaction(t1)} catch(e) {r = e}
    return r
